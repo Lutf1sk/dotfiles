6f61f14a4438119c2ce1f4cc965b9c8d911a888c
Alex Edin <lutfisk@lutfisk.net>
2021-05-30 13:33:57 +0200

    Added install.sh, which installs all config files in the correct locations

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+mkdir ~/.config
+
+# user-dirs
+cp user-dirs.dirs ~/.config/
+mkdir ~/img
+mkdir ~/doc
+mkdir ~/src
+mkdir ~/audio
+mkdir ~/dl
+mkdir ~/desktop
+
+# termite
+mkdir ~/.config/termite
+cp termite/config ~/.config/termite/
+
+# img
+cp img/* ~/img/
+
+# fish
+mkdir ~/.config/fish
+cp fish/config.fish ~/.config/fish/
+
+# i3
+mkdir ~/.config/i3
+cp i3/config i3/i3status.conf ~/.config/i3/
