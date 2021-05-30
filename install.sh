296289385581894c476bd5c796acf1d6678302eb
Alex Edin <lutfisk@lutfisk.net>
2021-05-30 14:47:23 +0200

    Add script for installing the programs i need the most

--- install.sh
+++ install.sh
@@ -1,33 +1,54 @@
 #!/bin/sh
 
-mkdir ~/.config
+base="xorg i3-gaps i3status termite fish base-devel nasm feh pulseaudio"
 
-# user-dirs
-cp user-dirs.dirs ~/.config/
-mkdir ~/img
-mkdir ~/doc
-mkdir ~/src
-mkdir ~/audio
-mkdir ~/dl
-mkdir ~/desktop
+full="$base qtcreator lxrandr firefox networkmanager neofetch xfce4-taskmanager pavucontrol volumeicon"
 
-# termite
-mkdir ~/.config/termite
-cp termite/config ~/.config/termite/
+if [ $1 == "base" ]; then
+	sudo pacman -S --noconfirm --needed $base
+fi
 
-# img
-cp img/* ~/img/
+if [ $1 == "full" ]; then
+	sudo pacman -S --noconfirm --needed $full
+fi 
 
-# fish
-mkdir ~/.config/fish
-cp fish/config.fish ~/.config/fish/
+if [ $1 == "yay" ]; then
+	sudo pacman -S --noconfirm --needed go git base-devel
+	git clone "https://www.github.com/Jguer/yay.git"
+	make -C yay
+	sudo make -C yay install
+fi
 
-# i3
-mkdir ~/.config/i3
-cp i3/config i3/i3status.conf ~/.config/i3/
-
-# I'm going to leave this commented to prevent myself from accidentally overwriting
-# my xinitrc when i don't want to.
-# xinitrc
-# cp xinitrc ~/.xinitrc
+if [ $1 == "config" ]; then
+	mkdir ~/.config
+	
+	# user-dirs
+	cp user-dirs.dirs ~/.config/
+	mkdir ~/img
+	mkdir ~/doc
+	mkdir ~/src
+	mkdir ~/audio
+	mkdir ~/dl
+	mkdir ~/desktop
+	
+	# termite
+	mkdir ~/.config/termite
+	cp termite/config ~/.config/termite/
+	
+	# img
+	cp img/* ~/img/
+	
+	# fish
+	mkdir ~/.config/fish
+	cp fish/config.fish ~/.config/fish/
+	
+	# i3
+	mkdir ~/.config/i3
+	cp i3/config i3/i3status.conf ~/.config/i3/
+	
+	# I'm going to leave this commented to prevent myself from accidentally overwriting
+	# my xinitrc when i don't want to.
+	# xinitrc
+	# cp xinitrc ~/.xinitrc
+fi
 
