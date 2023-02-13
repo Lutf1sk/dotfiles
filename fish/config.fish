e60dae261135f06d09eff62e42a7919ee93f6159
Alex Edin <lutfisk@lutfisk.net>
2023-02-13 08:46:14 +0100

    Add new git-related aliases

--- fish/config.fish
+++ fish/config.fish
@@ -1,31 +1,35 @@
-alias mkd='mkdir'
-alias mkf='touch'
-alias mk='make'
-alias cmk='cmake .'
+ls "ls --color=always"
 
-alias pac='sudo pacman -S'
-alias pacs='sudo pacman -Ss'
-alias pacu='sudo pacman -Syu'
-alias pacy='sudo pacman -Syy'
-alias pacr='sudo pacman -Rs'
+alias mkd="mkdir"
+alias mkf="touch"
+alias mk="make"
 
-alias yays='yay -Ss'
-alias yayu='yay -Syu'
-alias yayy='yay -Syy'
-alias yayr='yay -Rs'
+alias pac="sudo pacman -S"
+alias pacs="pacman -Ss"
+alias pacu="sudo pacman -Syu"
+alias pacy="sudo pacman -Syy"
+alias pacr="sudo pacman -Rs"
 
-alias gadd='git add'
-alias gmv='git mv'
-alias grm='git rm'
-alias gcom='git commit'
-alias gdiff='git diff'
-alias gpush='git push origin'
-alias gpull='git pull origin'
-alias gstat='git status'
-alias gclone='git clone --recursive'
+alias yays="yay -Ss"
+alias yayu="yay -Syu"
+alias yayy="yay -Syy"
+alias yayr="yay -Rs"
 
-alias cls='clear'
-alias zip='zip -r'
+alias gadd="git add"
+alias gmv="git mv"
+alias grm="git rm"
+alias gcom="git commit"
+alias gdiff="git diff"
+alias gpush="git push origin"
+alias gpull="git pull origin"
+alias gstat="git status"
+alias gclone="git clone --recursive"
+alias glog="git log"
+alias gamnd="git commit --amend"
+alias grst="git reset"
+
+alias cls="clear"
+alias zip="zip -r"
 
 function fish_greeting
 	echo ""
