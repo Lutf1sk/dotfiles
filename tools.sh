ca68b7926eaa65085fb68a1cfcaf9726cae741f5
Lutf1sk <lutfisk@lutfisk.net>
2023-10-15 16:35:34 +0200

    add colors to bashrc and add lcloc to tools.sh

--- tools.sh
+++ tools.sh
@@ -2,8 +2,14 @@
 
 git clone --recursive https://github.com/Lutf1sk/led
 sudo make -C led install
+mkdir -p ~/.config/led
+cp led/example.conf ~/.config/led/led.conf
 sudo rm -r led
 
 git clone --recursive https://github.com/Lutf1sk/ldasm
 sudo make -C ldasm install
 sudo rm -r ldasm
+
+git clone --recursive https://github.com/Lutf1sk/lcloc
+sudo make -C lcloc install
+sudo rm -r lcloc
--- void.sh
+++ void.sh
@@ -5,11 +5,12 @@ sudo xbps-install -Su void-repo-multilib void-repo-multilib-nonfree void-repo-no
 base="psmisc"
 dev="git make gcc clang nasm"
 mesa="mesa mesa-dri mesa-vaapi mesa-vdpau"
-session="polkit dbus elogind elogind-dbus"
-window="sway xorg-server-xwayland xorg-fonts"
+terminal="alacritty bash"
+session="polkit dbus elogind dbus-elogind"
+window="sway xorg-server-xwayland xorg-fonts i3status dmenu"
 xdg="xdg-desktop-portal xdg-user-dirs xdg-utils"
 
-sudo xbps-install -Su $base $dev $mesa $session $window $xdg
+sudo xbps-install -Su $base $dev $mesa $terminal $session $window $xdg
 
 # enable services
 sudo ln -s /etc/sv/dbus /var/service/
