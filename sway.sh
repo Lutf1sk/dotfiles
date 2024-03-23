2245f343be2fedaebcec35028da794c23ae2e3d8
Alex Edin <lutfisk@lutfisk.net>
2024-03-23 15:39:43 +0100

    automatically kill pipewire processes after sway exits

--- sway.sh
+++ sway.sh
@@ -8,7 +8,11 @@ export MOZ_ENABLE_WAYLAND=1
 export WLR_NO_HARDWARE_CURSORS=1
 export WLR_DRM_DEVICES=/dev/dri/card0
 
+export QT_QPA_PLATFORM_PLUGIN_PATH=/usr/lib/qt5/plugins
+export QT_STYLE_OVERRIDE=kvantum
+
 export SWAY_LOG=/tmp/sway_log
 exec >$SWAY_LOG 2>&1
 
-exec sway --unsupported-gpu --verbose
+sway --unsupported-gpu --verbose
+killall -w wireplumber pipewire
