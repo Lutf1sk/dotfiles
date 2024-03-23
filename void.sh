2be05c23d845cd19b190d001e2da9fe2d20a9a24
Alex Edin <lutfisk@lutfisk.net>
2024-03-23 14:29:28 +0100

    add wireplumber installation to void.sh

--- void.sh
+++ void.sh
@@ -11,14 +11,24 @@ terminal="alacritty bash"
 session="polkit dbus elogind dbus-elogind"
 window="sway xorg-server-xwayland xorg-fonts i3status dmenu"
 xdg="xdg-desktop-portal xdg-user-dirs xdg-utils"
+audio="pipewire alsa-pipewire libjack-pipewire libspa-bluetooth"
 
-sudo xbps-install -Suy $util $dev $mesa $terminal $session $window $xdg
+sudo xbps-install -Suy $util $dev $mesa $terminal $session $window $xdg $audio
 
 # ----- enable services
-sudo ln -s /etc/sv/dbus /var/service/
+sudo ln -sf /etc/sv/dbus /var/service/
 sudo sv up dbus
-sudo ln -s /etc/sv/polkitd /var/service/
+sudo ln -sf /etc/sv/polkitd /var/service/
 sudo sv up polkitd
+
+# ----- set up pipewire
+sudo mkdir -p /etc/pipewire/pipewire.conf.d
+sudo ln -sf /usr/share/examples/wireplumber/10-wireplumber.conf /etc/pipewire/pipewire.conf.d/
+sudo ln -sf /usr/share/examples/pipewire/20-pipewire-pulse.conf /etc/pipewire/pipewire.conf.d/
+
+sudo mkdir -p /etc/alsa/conf.d
+sudo ln -sf /usr/share/alsa/alsa.conf.d/50-pipewire.conf /etc/alsa/conf.d/
+sudo ln -sf /usr/share/alsa/alsa.conf.d/99-pipewire-default.conf /etc/alsa/conf.d/
 
 # ----- copy sway launch script
 cp sway.sh ~/sway.sh
