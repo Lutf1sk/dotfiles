8db79c4c5cb21d6c6ebeb6c6bcf9012b60a24bd5
Alex Edin <lutfisk@lutfisk.net>
2024-04-08 05:16:02 +0200

    add lls to tools.sh and bashrc

--- tools.sh
+++ tools.sh
@@ -13,3 +13,7 @@ sudo rm -r ldasm
 git clone --recursive https://github.com/Lutf1sk/lcloc
 sudo make -C lcloc install
 sudo rm -r lcloc
+
+git clone --recursive https://github.com/Lutf1sk/lls
+sudo make -C lls install
+sudo rm -r lls
