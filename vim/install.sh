cbefcfb6210db3562c585a5a9e27e974e11a81c9
Alex Edin <lutfisk@lutfisk.net>
2025-06-07 19:34:55 +0200

    add ctrlp to vim install script

--- vim/install.sh
+++ vim/install.sh
@@ -12,6 +12,11 @@ elif [ "$MODE" == "arch"   ]; then
 	sudo pacman -S vim
 fi
 
+if [ ! -d "$HOME/.vim/ctrlp.vim" ]; then
+	mkdir -p "$HOME/.vim"
+	git clone https://github.com/kien/ctrlp.vim "$HOME/.vim/ctrlp.vim"
+fi
+
 mkdir -p "$TARGET_DIR"
 cp vimrc "$TARGET"
 cat "vimrc_$MODE" >> "$TARGET" || true
