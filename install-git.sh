578bf32a990f4feb78278549a80b1e02190fa421
Alex Edin <lutfisk@lutfisk.net>
2025-06-07 13:12:31 +0200

    sign git commits with gpg key

--- install-git.sh
+++ install-git.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 if   [ "$MODE" == "alpine" ]; then
-	doas apk add git vim
+	doas apk add git vim gpg gpg-agent
 elif [ "$HOME" == "void"   ]; then
 	sudo xbps -S git vim
 elif [ "$HOME" == "arch"   ]; then
-	sudo pacman -S git vim
+	sudo pacman -S git vim gpg gpg-agent
 fi
 
 git config --global core.editor vim
+git config --global commit.gpgsign true
 git config --global pull.rebase false
 git config --global credential.helper store
 git config --global init.defaultBranch main
